0eca3008a0d491655e072aa1af13b3bb7aa41efe
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-08-13 15:19:03 +0530

    done

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -4,4 +4,18 @@
 
 def mostfrequentdigit(n):
 	# your code goes here
-	pass
+	# pass
+	if len(str(n))==len(set(str(n))):
+		return int(min(str(n)))
+	res=dict()
+	for i in range(len(str(n))):
+		res[str(n)[i]]=str(n).count((str(n))[i])
+	print(res)
+	ress=[]
+	for key,i in res.items():
+		if int(i) == int(max(res.values())):
+			ress.append(int(key))
+	if len(ress)<2:
+		return ress[0]
+	else:
+		return min(ress)
