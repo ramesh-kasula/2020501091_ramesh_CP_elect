89c7fd61743b2e2070de141ab31ee5fb7d2614ed
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-08-17 15:02:06 +0530

    done

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -7,6 +7,11 @@
 
 
 
-
+import math
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	if row<0 or col<0 or col>row:
+		return 0
+	x=math.factorial(row)
+	y=math.factorial(col)
+	z=y*math.factorial(row-col)
+	return int(x//z)
