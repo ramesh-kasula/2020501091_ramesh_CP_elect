29e8cd9d4ebbef23de01d0fc9589b8bbd9ef13e2
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-08-18 13:07:38 +0530

    done

--- 02-fabricyards-Python/fabricyards.py
+++ 02-fabricyards-Python/fabricyards.py
@@ -14,8 +14,12 @@
 
 def fabricyards(inches):
 	# Your code goes here...
-	return 1
+	if inches%36==0:
+		return inches//36
+	return (inches//36)+1
 
 def fabricexcess(inches):
 	# Your code goes here...
-	return 1
+	if inches%36==0:
+		return 0
+	return 36-(inches%36)
