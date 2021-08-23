341a52aafef787fc981e12797e3d1c60e7b5f462
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-08-23 12:20:09 +0530

    done

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,8 +5,16 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
-
-
-
-
+    if len(m1[0])!=len(m2):
+        return None
+    res=[]
+    for i in range(len(m1)):
+        res+=[[0]*len(m2[0])]
+    # res=[[0]*len(m2[0])]*len(m1)
+    if len(m1[0])==len(m2):
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m2)):
+                    res[i][j]+=m1[i][k]*m2[k][j]
+        print(res)
+        return res
