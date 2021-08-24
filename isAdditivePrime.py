51ace1b09a7240439117fc5d00452ff47903e078
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-08-24 14:55:39 +0530

    done

--- isAdditivePrime.py
+++ isAdditivePrime.py
@@ -15,3 +15,38 @@
 # assert (isAdditivePrime(67) == True)
 # assert (isAdditivePrime(97) == False)
 # print("All test cases passed... :-)")
+
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
+def isAdditivePrime(n):
+    if prime(n)==False:
+        return False
+    sum=0
+    while(n>0):
+        sum+=n%10
+        n=n//10
+    if prime(sum):
+        return True
+    return False
+
+
+# Here are sample test cases:
+assert (isAdditivePrime(2) == True)
+assert (isAdditivePrime(3) == True)
+assert (isAdditivePrime(5) == True)
+assert (isAdditivePrime(13) == False)
+assert (isAdditivePrime(23) == True)
+assert (isAdditivePrime(29) == True)
+assert (isAdditivePrime(41) == True)
+assert (isAdditivePrime(98) == False)
+assert (isAdditivePrime(198) == False)
+assert (isAdditivePrime(290) == False)
+assert (isAdditivePrime(67) == True)
+assert (isAdditivePrime(97) == False)
+print("All test cases passed... :-)")
