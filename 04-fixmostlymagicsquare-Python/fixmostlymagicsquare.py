4225dd18d158bf9ecdabae4583fe855b9bee51ab
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-08-27 15:01:35 +0530

    done

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -8,3 +8,32 @@
 
 def fixmostlymagicsquare(L):
 	# Your code goes here
+	rSL=[]
+	sr=0
+	for i in range(len(L)):
+		sr+=sum(L[i])
+		rSL.append(sr)
+		sr=0
+	for i in range(len(rSL)):
+		if rSL.count(rSL[i])==1:
+			row=i
+	# print(row)
+	cr=0
+	cSL=[]
+	for i in range(len(L)):
+		for j in range(len(L)):
+			cr+=L[j][i]
+		cSL.append(cr)
+		cr=0
+	# print(cSL)
+	for i in range(len(cSL)):
+		if cSL.count(cSL[i])==1:
+			col=i
+	# print(row,col)
+	if row!=len(L)-1:
+		d=sum(L[row])-sum(L[row+1])
+	else:
+		d=sum(L[row])-sum(L[row-1])
+	L[row][col]-=d
+	return L
+print(fixmostlymagicsquare([[16, 3, 2, 13], [5, 10, 11, 18], [9, 6, 7, 12],[4, 15, 14, 1]]))
