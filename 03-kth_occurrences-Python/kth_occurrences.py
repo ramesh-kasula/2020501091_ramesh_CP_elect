e2cb19d8c1128413b6302b34502eaf2772503a20
ramesh-kasula <kaasularamesh389@msitprogram.net>
2021-09-01 20:53:42 +0530

    done

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -2,9 +2,28 @@
 # frequent character in the string. If there are multiple characters that 
 # can account as K-th the most frequent character then, print any one of them.
 
-
-
 def fun_kth_occurrences(s, n):
-	return 'a'
+	d=dict()
+	for i in s:
+		if not i.isalpha():
+			pass
+		elif(i in d):
+			d[i]+=1
+		else:
+			d[i]=1
+	c=1
+	while c<n:
+		maxn=max(d.values())
+		for k,v in d.items():
+			if maxn==v:
+				d[k]=0
+				c+=1
+				print(d)
+	for k,v in d.items():
+		if v==max(d.values()):
+			return k
+	return ""
 
 
+
+print(fun_kth_occurrences("helllo woorld",2))
